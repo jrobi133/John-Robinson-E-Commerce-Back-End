1706746903cb61108d17c31f8507718f20ec9eb2
John Robinson <johnarobinson133@gmail.com>
2020-12-11 11:04:28 -0700

    took out extra models and added foreign keys to index.js file in models folder

--- models/index.js
+++ models/index.js
@@ -1,7 +1,4 @@
-const Category = require('./Category');
-const Product = require('./Product');
-const ProductTag = require('./ProductTag');
-const Tag = require('./Tag');
+
 // import models
 const Product = require('./Product');
 const Category = require('./Category');
@@ -9,12 +6,26 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey:'category_id',
+});
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+});
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  as: 'product_tags',
+  foreignKey: 'product_id'
+});
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  as: 'tag_products',
+  foreignKey: 'tag_id'
+});
 
 module.exports = {
   Product,
